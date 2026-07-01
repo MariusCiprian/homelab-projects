0820d1b67ca3e7e1ec3fb61464430496dca40863
JMC-Desktop <jalbamariusciprian@yahoo.com>
2026-07-01 12:35:28 +0000

    last commit for today

--- 01-linux-system-info/main.py
+++ 01-linux-system-info/main.py
@@ -1,53 +1,41 @@
 import subprocess
 
-#hostname = input("Write the hostname:  ")
-#current_user = input("Write the current user:  ")
-print()
-print()
 print("=" * 35)
 print(" Linux System Information")
 print("=" * 35)
 print()
-print()
 
-hostname= subprocess.run(
+# Hostname
+hostname = subprocess.run(
     ["hostname"],
     capture_output=True,
     text=True,
-    );
-current_user= subprocess.run(
+)
+
+# Current User
+current_user = subprocess.run(
     ["whoami"],
     capture_output=True,
     text=True,
-);
+)
 
-os_version= subprocess.run(
-     ["cat", "/etc/os-release"],
-     capture_output=True,
-     text=True,
+# Operating System
+os = subprocess.run(
+    ["cat", "/etc/os-release"],
+    capture_output=True,
+    text=True,
+)
 
-);
+os_version = ""
 
-# lines= os_version.stdout.splitlines()
-# #print(lines);
-# for line in lines :
-#     if line.startswith("PRETTY_NAME"):
-#         print(line);
+lines = os.stdout.splitlines()
 
-text = 'PRETTY_NAME="Ubuntu 25.10"'
+for line in lines:
+    if line.startswith("PRETTY_NAME"):
+        parts = line.split("=")
+        os_version = parts[1].strip('"')
+        break
 
-parts = text.split("=");
-print(parts[1]);
-
-#os_version = os.VERSION_ID;
-#print(os.split)
-#print(os_version);
-# hostname = result.stdout;
-# print(hostname)
-# print(result)
-# print(result.stdout)
-# #print("Hostname            : " + hostname)
-# #print("Current User        : " + current_user)
-# print("Hostname:" + hostname.stdout)
-# print("Current user:" + current_user.stdout);
-# print("OS Version:" + os_version);
+print("Hostname      :", hostname.stdout.strip())
+print("Current User  :", current_user.stdout.strip())
+print("OS Version    :", os_version)
